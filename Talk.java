429d36b2ebc4d63bd8143f42bf4bcc0ef592b130
BruceHib <huangyoupeng93@gmail.com>
2016-09-16 16:47:30 -0500

    Revert "IU-2016.2.4 <Bruce@youpengs-macbook-air.local Merge branch 'master'"
    This reverts commit 0c6341c8d872b8bd657b1f4f1b6c1579a5e59ca3, reversing
    changes made to c3c9b1f5e0141b9989d7b5d52ccb462ef690b883.

--- Talk.java
+++ Talk.java
@@ -17,115 +17,119 @@ public class Talk
                             serverName = args[1];
                             try {
                                 serverPortNumber = Integer.parseInt(args[2]);
-                            } catch (Exception e) {
+                            }catch(Exception e){
                                 System.out.println("Port number input error, has to be integer.");
                                 System.exit(-1);
                             }
-                        } else {
+                        }else {
                             try {
                                 serverPortNumber = Integer.parseInt(args[1]);
-                            } catch (Exception e) {
+                            }catch(Exception e){
                                 System.out.println("Port number input error, has to be integer.");
                                 System.exit(-1);
                             }
                         }
-                    }
-                    System.out.println("Starting TalkClient");
-                    String MyMessage;
-                    try {
-                        Socket socket = new Socket(serverName, serverPortNumber);
-                        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-                        BufferedReader stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                        while (true) {
-                            if (stdIn.ready()) {
-                                System.out.println("Server:" + stdIn.readLine());
-                            }
-                            if (in.ready()) {
-                                MyMessage = in.readLine();
-                                if (MyMessage.equals("STATUS")) {
-                                    System.out.println("Host name is: " + serverName + "\n" + "Port number is: " + serverPortNumber);
-                                } else{
-                                    out.println(MyMessage);
+                        System.out.println("Starting TalkClient");
+                        String MyMessage;
+
+                        try {
+                            Socket socket = new Socket(serverName, serverPortNumber);
+                            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                            BufferedReader stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                            while (true) {
+                                if (stdIn.ready()) {
+                                    System.out.println("Server:" + stdIn.readLine());
+                                }
+                                if (in.ready()) {
+                                    MyMessage = in.readLine();
+                                    if (MyMessage.equals("STATUS")) {
+                                        System.out.println("Host name is: " + serverName + "\n" + "Port number is: " + serverPortNumber);
+                                    } else{
+                                        out.println(MyMessage);
+                                    }
                                 }
                             }
+                        } catch (UnknownHostException e) {
+                            System.out.println("Unknown host:" + serverName);
+                            System.exit(1);
+                        } catch (IOException e) {
+                            System.out.println("No I/O");
+                            System.exit(1);
                         }
-                    } catch (UnknownHostException e) {
-                        System.out.println("Unknown host:" + serverName);
-                        System.exit(1);
-                    } catch (IOException e) {
-                        System.out.println("No I/O");
-                        System.exit(1);
                     }
-                } else if (args[0].equals("-s")) {
+                }else if (args[0].equals("-s")){
                     int serverPortNumber = 16466;
                     if (args[1] != null) {
                         try {
                             serverPortNumber = Integer.parseInt(args[1]);
-                        } catch (Exception e) {
+                        }catch(Exception e){
                             System.out.println("Port number input error, has to be integer.");
                             System.exit(-1);
                         }
-                    }
-                    System.out.println("Starting TalkServer with port number: " + serverPortNumber);
-                    BufferedReader stdIn = null;
-                    BufferedReader in = null;
-                    PrintWriter out = null;
-                    String message;
-                    Socket client = null;
-                    ServerSocket server = null;
-                    try {
-                        server = new ServerSocket(serverPortNumber);
-                        System.out.println("Server listening on port " + serverPortNumber);
-                    } catch (IOException e) {
-                        System.out.println("Could not listen on port " + serverPortNumber);
-                        System.exit(-1);
-                    }
-                    try {
-                        client = server.accept();
-                        System.out.println("Server accepted connection from " + client.getInetAddress());
-                    } catch (IOException e) {
-                        System.out.println("Accept failed on port " + serverPortNumber);
-                        System.exit(-1);
-                    }
-                    try {
-                        stdIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    } catch (IOException e) {
-                        System.out.println("Couldn't get an inputStream from the client");
-                        System.exit(-1);
-                    }
-                    try {
-                        out = new PrintWriter(client.getOutputStream(), true);
-                        in = new BufferedReader(new InputStreamReader(System.in));
-                    } catch (IOException e) {
-                        System.out.println("Read and Write error.");
-                        System.exit(-1);
-                    }
-                    try {
-                        while (true) {
-                            if (stdIn.ready()) {
-                                System.out.println("Client:" + stdIn.readLine());
-                            }
-                            if (in.ready()) {
-                                message = in.readLine();
-                                if (message.equals("STATUS")) {
-                                    System.out.println("Port number is: " + serverPortNumber);
-                                } else {
-                                    out.println(message);
+                        System.out.println("Starting TalkServer with port number: " + serverPortNumber);
+                        BufferedReader stdIn = null;
+                        BufferedReader in = null;
+                        PrintWriter out = null;
+                        String message;
+                        Socket client = null;
+                        ServerSocket server = null;
+
+                        try {
+                            server = new ServerSocket(serverPortNumber);
+                            System.out.println("Server listening on port " + serverPortNumber);
+                        } catch (IOException e) {
+                            System.out.println("Could not listen on port " + serverPortNumber);
+                            System.exit(-1);
+                        }
+                        try {
+                            client = server.accept();
+                            System.out.println("Server accepted connection from " + client.getInetAddress());
+                        } catch (IOException e) {
+                            System.out.println("Accept failed on port " + serverPortNumber);
+                            System.exit(-1);
+                        }
+                        try {
+                            stdIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        } catch (IOException e) {
+                            System.out.println("Couldn't get an inputStream from the client");
+                            System.exit(-1);
+                        }
+                        try {
+                            out = new PrintWriter(client.getOutputStream(), true);
+                            in = new BufferedReader(new InputStreamReader(System.in));
+                        } catch (IOException e) {
+                            System.out.println("Couldn't read your input and get socket output");
+                            System.exit(-1);
+                        }
+
+                        try {
+                            while (true) {
+                                if (stdIn.ready()) {
+                                    System.out.println("Client:" + stdIn.readLine());
+                                }
+                                if (in.ready()) {
+                                    message = in.readLine();
+                                    if (message.equals("STATUS")) {
+                                        System.out.println("Port number is: " + serverPortNumber);
+                                    } else {
+                                        out.println(message);
+                                    }
                                 }
                             }
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Read failed");
-                        System.exit(-1);
+                        } catch (IOException e) {
+                            System.out.println("Read failed");
+                            System.exit(-1);}
                     }
-                } else if (args[0].equals("-a")){
+                }else if (args[0].equals("-a")){
+
 
 
                 }
             } catch (Exception e){
                 System.out.println("error.");
             }
-        }else {System.out.println("Please give me a sweet command.");}
+        }else {System.out.println("Please give me command.");}
     }
 }
